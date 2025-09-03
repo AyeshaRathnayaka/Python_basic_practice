9c24a6050c36978a7c55cf298c04acf7e7391c2f
chamod0 <ayesharathnayaka35@gmail.com>
2025-09-03 10:28:08 +0530

    add 1

--- week3.py
+++ week3.py
@@ -1,7 +1,7 @@
 #find the largest among three numbers
 No1=input("First Number : ")
 No2=input("Second Number : ")
-No3=input("Third Number: ")
+No3=input("Third Number: 1")
 
 if No1>No2>No3 :
     print("Largest Number is "+ str(No1))
